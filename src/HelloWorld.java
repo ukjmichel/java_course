8804a3d808abb91980fc9dc0f4570248ed74bb63
ukjmichel <jmichel.uk@outlook.fr>
2024-09-11 12:56:10 +0200

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+// The HelloWorld class is declared as public, meaning it can be accessed by any other class.
+public class HelloWorld {
+
+	// The main method is the entry point of any Java program.
+	// When you run the program, this method is automatically executed.
+	// 'public' means it can be accessed outside the class.
+	// 'static' means it belongs to the class and can be called without creating an
+	// object of the class.
+	// 'void' means it doesn't return any value.
+	// 'String[] args' is an array that can hold command-line arguments passed when
+	// the program is executed.
+	public static void main(String[] args) {
+
+		// This line prints the text "Hello, World!" to the console.
+		// 'System.out' is used to access the standard output stream.
+		// 'println' is a method that prints the given string followed by a new line.
+		System.out.println("Hello, World!");
+	}
+}
